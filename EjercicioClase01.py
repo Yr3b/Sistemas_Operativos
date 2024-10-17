3f59dc6e61b2a4b70577f58ffbad7c311ad7e595
Gonzalo <gonzasoler.20@gmail.com>
2024-10-17 19:45:10 -0300

    Add class examples

---
+++ EjercicioClase01.py
@@ -0,0 +1,5 @@
+# Crea un programa en python que simule el acceso de varios usuarios a una caja de ahorro compartida, 
+# Cada usuario (Representado como un hilo) intentara realizar depositos en la caja de ahorro. 
+# Sin embargo, para evitar inconsistencias en el saldo total, es necesario implementar un mecanismo que asegure 
+# que un solo usuario pueda acceder a la caja de ahorro a la vez.
+
